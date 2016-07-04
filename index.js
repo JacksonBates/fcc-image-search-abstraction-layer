fbfbee5b8c8b371281f6b58acdbb3fbcd584f6b9
JacksonBates <malgalin@gmail.com>
2016-07-04 19:32:38 +1000

    Adds remote mongoDB and manages connection

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 var express = require( 'express' );
 var mongo = require( 'mongodb' );
 var path = require( 'path' );
-
+var mongo = mongodb.MongoClient;
+var mongoUserPsw = process.env.MONGO_USER_PSW;
 var app = express();
+var url = 'mongodb://' +
+  mongoUserPsw +
+  '@ds011725.mlab.com:11725/fcc-image-search-abstraction-layer'
 
-app.use( express.static( path.join( __dirname , '/public' )));
 
-// Use the routes specified in the routes file
-app.use( require( './routes' ));
-app.set( 'port', ( process.env.PORT || 5000 ));
-app.set( 'views', path.join( __dirname, '/views' ));
-app.set( 'view engine', 'ejs' );
+mongo.connect( url, function( err, db ) {
+  if (err) {
+    console.log( 'Unable to connect to database' );
+    throw err;
+  }
+  console.log( 'Successfully connected to the database' );
 
-// Start node server
-app.listen( app.get( 'port' ), function() {
-  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  // Pass db with each request
+  app.use( function( req, res, next ) {
+    req.db = db;
+    next();
+  });
+
+  app.use( express.static( path.join( __dirname , '/public' )));
+
+  // Use the routes specified in the routes file
+  app.use( require( './routes' ));
+  app.set( 'port', ( process.env.PORT || 5000 ));
+  app.set( 'views', path.join( __dirname, '/views' ));
+  app.set( 'view engine', 'ejs' );
+
+  // Start node server
+  app.listen( app.get( 'port' ), function() {
+    console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
+
 });
