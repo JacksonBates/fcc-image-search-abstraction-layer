88593b8284a504402134f0d82498c79e7c9f5d1e
JacksonBates <malgalin@gmail.com>
2016-07-04 22:36:20 +1000

    Adds incomplete ajax request

--- routes.js
+++ routes.js
@@ -1,15 +1,25 @@
-var express = require('express');
+var express = require( 'express' );
 var router = express.Router();
+var imgCliID = process.env.IMGUR_CLIENT_ID;
 
 // Homepage for the app
-router.get( '/', function(req, res) {
-  res.render('pages/home');
+router.get( '/', function( req, res ) {
+  res.render( 'pages/home' );
 });
 
-// Cathces invalid HTTP requests and returns 404
-router.get('*', function(req, res) {
-  res.status(404);
-  res.render('pages/not-found');
+router.get( '/api/imagesearch/:SEARCH',  function( req, res ) {
+  var search = req.params.SEARCH;
+  console.log(imgCliID);
+  res.render( 'pages/search-results', {
+    imgCliID: imgCliID,
+    search: search
+    });
+});
+
+// Catches invalid HTTP requests and returns 404
+router.get( '*' , function( req, res ) {
+  res.status( 404 );
+  res.render( 'pages/not-found' );
   res.end();
 });
 
